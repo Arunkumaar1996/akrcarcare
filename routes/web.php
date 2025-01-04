534b566a9bd38b61c0ddac141f4e33faa299e8e8
Arun <arun199606@gmail.com>
2025-01-04 22:47:45 +0530

    added service plane form and collect the data in request

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,38 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/serviceForm', function (Request $request) {
+    dd($request);
+})->name('serviceForm.store');
+Route::get('/selected-plan/{id}', function ($id) {
+    $data = '[
+        {
+            "id": 1,
+            "plan_name": "Basic Plan",
+            "price": 9.99
+        },
+        {
+            "id": 2,
+            "plan_name": "Standard Plan",
+            "price": 19.99
+        },
+        {
+            "id": 3,
+            "plan_name": "Premium Plan",
+            "price": 29.99
+        }
+    ]';
+    
+   $getPlan = collect(json_decode($data))->where('id',$id)->first();
+   return view('frontend.pages.price-select-form',compact('getPlan'));
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
